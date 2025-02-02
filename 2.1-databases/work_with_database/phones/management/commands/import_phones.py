b633078677db8669715e1758dcedd8304fe3c62d
Kirill Usoltsev <tem9n@mail.ru>
2025-02-02 13:25:26 +0500

    phones ready

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -14,4 +14,7 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            # p = Phone(id=phone.get('id'), name=phone.get('name'), image=phone.get('image'), price=phone.get('price'),
+            #           release_date=phone.get('release_date'), lte_exists=phone.get('lte_exists'))
+            p = Phone(**phone)
+            p.save()
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort')
     template = 'catalog.html'
-    context = {}
+    phones_objects = Phone.objects.all()
+    phones = [p for p in phones_objects]
+    if sort == 'name':
+        phones.sort(key=lambda x: x.name)
+    if sort == 'min_price':
+        phones.sort(key=lambda x: x.price)
+    if sort == 'max_price':
+        phones.sort(key=lambda x: x.price, reverse=True)
+    context = {
+        'phones': phones,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    p = Phone.objects.get(slug=slug)
+    context = {'phone': p}
     return render(request, template, context)
